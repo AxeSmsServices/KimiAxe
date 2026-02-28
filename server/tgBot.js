38474f25c8fb7b315fba8c415cd42cf7ba6a7d0e
AxeSmsServices <legal@axesms.org>
2026-02-28 08:07:16 -0500

    Create Telegram bot with command support for managing posts across platforms.

--- server/tgBot.js
+++ server/tgBot.js
@@ -1,17 +1,68 @@
-// Telegram Bot Configuration
+'use strict';
 
-const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Replace with your actual token
-const token = '7991955321:AAEv-uBBtZEyLzU4wavjM_F9yo5mHorEci0';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Manage posts across social platforms
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Welcome to the bot!');
+// Command to start the bot
+bot.start((ctx) => {
+    ctx.reply('Welcome to the Social Media Manager Bot! Use /help to see all commands.');
 });
 
-// More configurations and managing posts can be added below
+// Command to show help
+bot.command('help', (ctx) => {
+    ctx.reply(`Available commands:\n/tech_post - Manage tech posts\n/finance_post - Manage finance posts\n/entertainment_post - Manage entertainment posts\n/showAll - Show all posts\n/add_post {platform} {content} - Add a new post to a platform\n/remove_post {id} - Remove a post by ID\n`);
+});
+
+// Command to add a post
+bot.command('add_post', (ctx) => {
+    const { message } = ctx;
+    const args = message.text.split(' ').slice(1);
+    if (args.length < 2) {
+        return ctx.reply('Usage: /add_post {platform} {content}.');
+    }
+    const [platform, ...contentArr] = args;
+    const content = contentArr.join(' ');
+    // TODO: Add functionality to store the post
+    ctx.reply(`Post added to ${platform}: ${content}`);
+});
+
+// Command to remove a post
+bot.command('remove_post', (ctx) => {
+    const { message } = ctx;
+    const postId = message.text.split(' ')[1];
+    if (!postId) {
+        return ctx.reply('Usage: /remove_post {id}.');
+    }
+    // TODO: Add functionality to remove the post
+    ctx.reply(`Post with ID ${postId} removed.`);
+});
+
+// Command to show all posts
+bot.command('showAll', (ctx) => {
+    // TODO: Retrieve and show all posts
+    ctx.reply('Showing all posts...');
+});
+
+// Command to manage platform-specific posts
+bot.command('tech_post', (ctx) => {
+    ctx.reply('Managing Tech Posts...');
+});
+
+bot.command('finance_post', (ctx) => {
+    ctx.reply('Managing Finance Posts...');
+});
+
+bot.command('entertainment_post', (ctx) => {
+    ctx.reply('Managing Entertainment Posts...');
+});
+
+// Launch the bot
+bot.launch();
+console.log('Bot is launched and running...');
+
+process.on('SIGINT', () => {
+    bot.stop('SIGINT');
+});
+process.on('SIGTERM', () => {
+    bot.stop('SIGTERM');
+});
